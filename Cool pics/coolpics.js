7fd8a55df623668a9c01e5a9ce0968808bd77ab7
Maxwride <maxwride@byui.edu>
2025-01-31 09:03:38 -0700

    update 2

---
+++ Cool pics/coolpics.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function handleResize() {
+      const button = document.querySelector(".menu");
+      if (window.innerWidth > 1000) {
+        button.classList.remove("hide");
+      } else {
+        button.classList.add("hide");
+      }
+    }
+  
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  
+    function viewerTemplate(pic, alt) {
+      return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+      </div>`;
+    }
+  
+    function viewHandler(event) {
+      let body = document.querySelector('body');
+      let html = viewerTemplate('norris-sm.jpeg', 'Review Image');
+      body.insertAdjacentHTML('afterbegin', html);
+  
+      setTimeout(() => {
+        document.querySelector('.close-viewer').addEventListener('click', (event) => {
+          event.stopPropagation(); // Stop the click event from propagating
+          document.querySelector('.viewer').remove();
+        });
+      }, 0);
+    }
+  
+    document.querySelectorAll('.gallery img').forEach(img => {
+      img.addEventListener('click', viewHandler);
+    });
+  });
+  
+  
---
+++ debugging.js
@@ -0,0 +1,15 @@
+// const PI = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// radius = 4;
+// area = radius * radius * PI;
+
+
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  const area = radius * radius * PI;
+}
+area = circleArea(3);
+console.log(area);
